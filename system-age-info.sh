8c63b4a9a3e87d0bd34d86b4d6a66ca28ddba9cd
Jorink <maxjorink@gmail.com>
2026-06-07 14:39:10 +0200

    Changed greeting calculations

--- system-age-info.sh
+++ system-age-info.sh
@@ -14,6 +14,7 @@ usage_and_die () {
     echo "Commands:" >&2
     echo "  birth ... See when the system was installed. (Based on when '/' was created.)" >&2
     echo "  age   ... See system age. (Duration since birth.)" >&2
+	echo "  counted ... See syste age. (But numbers add up instead of the same value shown in different formats.)" >&2
     echo "  combined ... See system age in one line. (Useful for fastfetch.)" >&2
 
     exit 1
@@ -23,7 +24,11 @@ log_key_val () {
     echo -e "\033[0;33m$1:\033[0m $2\033[0m"
 }
 
-log_key_val_noclr () {
+log_key_val_counted () {
+	echo -e "\033[0;33m$1\033[0m $2\033[0m"
+}
+
+log_key_val_combined () {
     echo -e "\033[0;m$1\033[0m $2\033[0m"
 }
 
@@ -54,9 +59,31 @@ elif [[ "$display_mode" == "combined" ]]; then
     duration_days="$(( ${duration_hours} / 24 ))"
     duration_months="$(( ${duration_days} / 30 ))"
     duration_years="$(( ${duration_days} / 365 ))"
-
     duration_combined="Years: $(( ${duration_days} / 365 ))|Months: $(( ${duration_days} / 365 ))|Days: $(( ${duration_hours} / 24 ))|Hours: $(( ${duration_mins} / 60 ))|Minutes: $(( ${duration_secs} / 60 ))|Seconds: $(( $(get_now_time) - $(get_creation_time) ))"
-    log_key_val_noclr "$duration_combined"
+
+    log_key_val_combined "$duration_combined"
+elif [[ "$display_mode" == "counted" ]]; then
+
+    total_secs="$(( $(get_now_time) - $(get_creation_time) ))"
+    total_mins="$(( ${total_secs} / 60 ))"
+    total_hours="$(( ${total_mins} / 60 ))"
+    total_days="$(( ${total_hours} / 24 ))"
+    total_months="$(( ${total_days} / 30 ))"
+	total_years="$(( ${total_days} / 365 ))"
+
+	duration_secs="$(( ${total_secs} - ${total_mins} * 60 ))"
+	duration_mins="$(( ${total_mins} - ${total_hours} * 60 ))"
+	duration_hours="$(( ${total_hours} - ${total_days} * 24 ))"
+	duration_days="$(( ${total_days} - ${total_months} * 30 ))"
+	duration_months="$(( ${total_months} - ${total_years} * 12 ))"
+
+	echo "System age:"	
+	log_key_val_counted "Seconds:" "$duration_secs"
+	log_key_val_counted "Minutes:" "$duration_mins"
+	log_key_val_counted "Hours:" "$duration_hours"
+	log_key_val_counted "Days:" "$duration_days"
+	log_key_val_counted "Months" "$duration_months"
+	log_key_val_counted "Years:" "$total_years"
 else
     usage_and_die
 fi
